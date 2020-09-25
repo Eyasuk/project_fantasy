2a2106f862f11f851d42fb27f469162a0d5db945
Eyasuk <53485615+Eyasuk@users.noreply.github.com>
2020-09-25 19:48:21 +0300

    progress
    Co-Authored-By: Yeabsira290 <49195982+Yeabsira290@users.noreply.github.com>

--- main.py
+++ main.py
@@ -6,14 +6,23 @@ from selenium.webdriver.support.ui import WebDriverWait
 PATH = r"C:\Users\user42\Desktop\gethub\project_fantasy\chromedriver.exe"
 driver=webdriver.Chrome(PATH)
 driver.get("https://users.premierleague.com")
-username=driver.find_element_by_id("ism-email")
-password=driver.find_element_by_id("ism-password")
-submit=""
+email="eyasukibru290@gmail.com"
+passw="startover123"
 try:
-    submit=WebDriveWait(driver,10).until(
-        EC.presence_of_element_located((By.CLASS,"btn-primary"))
+    submit=WebDriverWait(driver,100).until(
+        EC.presence_of_element_located((By.CLASS_NAME,"btn-primary"))
         )
+    username=driver.find_element_by_id("ism-email")
+    password=driver.find_element_by_id("ism-password")
+    username.send_keys(email)
+    password.send_keys(passw)
+    submit.send_keys(Keys.RETURN)
+    fantasy_page=WebDriverWait(driver,100).until(
+        EC.presence_of_element_located((By.LINK_TEXT,"Play Fantasy Premier League"))
+        )
+    fantasy_page.send_keys(Keys.RETURN)
+    
 except:
     driver.quit()
-    
-print(submit.text)
+ 
+
